f0e03904c541dcc96c81d5a307a9ec5acca4d813
Oprea Alexandru <oalexprea@yahoo.com>
2017-04-08 22:10:16 +0300

    Init Commit
    UI Sample

---
+++ Assets/Troops/Scripts/TroopInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TroopInfo : MonoBehaviour
+{
+    public Sprite troopSprite;
+    public int count;
+
+}
