e6b5ed7218861581f80531bd70479b6d158b5e11
Oprea Alexandru <oalexprea@yahoo.com>
2017-04-12 21:33:08 +0300

    Hierachy update

---
+++ Assets/Managers/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour 
+{
+	public int bounds;
+
+	private void Start () 
+	{
+		
+	}
+	
+	private void Update () 
+	{
+			
+	}
+}
